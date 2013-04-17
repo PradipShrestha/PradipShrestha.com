9c9419d63d0b90dd6c6301f9336333d86943772a
Pradip Shrestha <pr.pradip@gmail.com>
2013-04-16 23:03:19 -0400

    Initial check in

---
+++ public_html/sulochana/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Sulochana Pradhan</title>
+</head>
+<body style="background-color:#ddd;">
+	<h2>
+		<?php
+		 echo 'Sulochana\'s personal site';
+		?>
+		<br />
+		Site is under construction. Please be patient.
+	</h2>
+</body>
